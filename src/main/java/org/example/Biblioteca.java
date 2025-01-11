1b9f17a726d44dd35e7faaf84538da8cf4720ba8
Santiago Peñafiel <santypenafiel8102005@gmail.com>
2025-01-11 11:48:56 -0500

    Segundo commit

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -14,6 +14,7 @@ public class Biblioteca {
     private int numeroLibros;
 
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
         int r = scan.nextInt();
 
         switch (r) {
@@ -21,6 +22,7 @@ public class Biblioteca {
                 r = scan.nextInt();
                 switch (r){
                     case 1: //añadir
+                        biblioteca.nuevoLibro(biblioteca.numeroLibros);
                         break;
 
                     case 2: //editar
@@ -51,6 +53,30 @@ public class Biblioteca {
 
 
     }
+
+    public void nuevoLibro(int numeroLibros) {
+
+        try {
+            System.out.print("Ingrese el titulo del libro que desea añadir : ");
+            String titulo = scan.nextLine();
+
+            System.out.print("Ingrese el nombre del autor de este libro : ");
+            String autor = scan.nextLine();
+
+            System.out.print("Ingrese el año de publicación del libro : ");
+            int anio = scan.nextInt();
+            scan.nextLine();
+
+            System.out.print("El libro " + titulo +
+                    "con el id " + numeroLibros+1
+                    +" se ha añadido a la lista de libros");
+
+            this.libros.add(new Libro(this.numeroLibros+1, titulo, autor, anio));
+
+        } catch (Exception e){
+            System.out.println("Ingrese valores válidos para añadir un libro");
+
+        }
 
 
 
--- src/main/java/org/example/Libro.java
+++ src/main/java/org/example/Libro.java
@@ -18,35 +18,6 @@ public class Libro {
         this.anio = anio;
     }
 
-    public Libro nuevoLibro(int numeroLibros) {
-
-        try {
-            System.out.print("Ingrese el titulo del libro que desea añadir : ");
-            String titulo = scan.nextLine();
-
-            System.out.print("Ingrese el nombre del autor de este libro : ");
-            String autor = scan.nextLine();
-
-            System.out.print("Ingrese el año de publicación del libro : ");
-            int anio = scan.nextInt();
-            scan.nextLine();
-
-            System.out.print("El libro " + titulo +
-                    "con el id " + numeroLibros+1
-                    +" se ha añadido a la lista de libros");
-
-            return new Libro(numeroLibros+1, titulo, autor, anio);
-        } catch (Exception e){
-            System.out.println("Ingrese valores válidos para añadir un libro");
-            return null;
-        }
-
-
-    }
-
-
-
-
     public int getId() {
         return id;
     }
